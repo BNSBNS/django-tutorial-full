74c55dff7d2550c94f98eac21c153fbc681683e1
bnsbns <y3kmarvin@gmail.com>
2021-05-15 01:42:55 +0800

    page2 done

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,29 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
+
+# NOTES ABOUT __STR__
+# Most notably, to display an object in the Django admin site and as the value inserted into a template when it displays an object. 
+# Thus, you should always return a nice, human-readable representation of the model from the __str__() method.
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
